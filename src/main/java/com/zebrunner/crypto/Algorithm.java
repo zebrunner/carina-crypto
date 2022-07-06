2df04187b8ca833561ac59ca896a0898134a6b54
Andrei Kamarouski <akamarouski@solvd.com>
2022-07-06 17:52:34 +0300

    Added algorithms

--- src/main/java/com/zebrunner/crypto/Algorithm.java
+++ src/main/java/com/zebrunner/crypto/Algorithm.java
@@ -1,9 +1,23 @@
 package com.zebrunner.crypto;
 
 public enum Algorithm {
-    AES("AES", "AES", 128),
-    DES("DES", "DES", 56),
-    DESEDE("DESede", "DESede", 168);
+    AES_ECB_PKCS5_PADDING("AES/ECB/PKCS5Padding", "AES", 128),  // tested
+    AES_ECB_NO_PADDING("AES/ECB/NoPadding", "AES", 128),
+    AES_ECB_ISO10126PADDING("AES/ECB/ISO10126Padding", "AES", 128),
+    AES_ECB_PKCS1PADDING("AES/ECB/PKCS1Padding", "AES", 128),
+
+    DES_ECB_PKS5_PADDING("DES/ECB/PKCS5Padding", "DES", 56),    // tested
+    DES_ECB_ISO10126PADDING("DES/ECB/ISO10126Padding", "DES", 56),    // tested
+    DES_ECB_PKCS1PADDING("DES/ECB/PKCS1Padding", "DES", 56),
+    DES_ECB_NO_PADDING("DES/ECB/NoPadding", "DES", 56), // tested
+
+    DESEDE_ECB_PKS5_PADDING("DESede/ECB/PKCS5Padding", "DESede", 168),
+    DESEDE_ECB_NO_PADDING("DESede/ECB/NoPadding", "DESede", 168),
+    DESEDE_ECB_ISO10126PADDING("DESede/ECB/ISO10126Padding", "DESede", 168),
+    DESEDE_ECB_PKCS1PADDING("DESede/ECB/PKCS1Padding", "DESede", 168),
+    RC2_ECB_PKCS1PADDING("RC2", "RC2", 168),// tested
+
+    ARCFOUR("ARCFOUR", "ARCFOUR", 128); // tested
 
     private final String name;
     private final int size;
