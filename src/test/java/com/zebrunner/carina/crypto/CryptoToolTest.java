a985772affd7349e140aef63a7c6431f1b916d33
Andrei Kamarouski <akamarouski@solvd.com>
2022-10-19 15:43:23 +0300

    change package of CryptoToolTest class

--- src/test/java/com/zebrunner/carina/crypto/CryptoToolTest.java
+++ src/test/java/com/zebrunner/carina/crypto/CryptoToolTest.java
@@ -1,11 +1,7 @@
-package com.zebrunner.crypto;
+package com.zebrunner.carina.crypto;
 
 import java.security.NoSuchAlgorithmException;
 
-import com.zebrunner.carina.crypto.Algorithm;
-import com.zebrunner.carina.crypto.CryptoTool;
-import com.zebrunner.carina.crypto.CryptoToolBuilder;
-import com.zebrunner.carina.crypto.SecretKeyManager;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
